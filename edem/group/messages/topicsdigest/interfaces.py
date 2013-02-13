5675a83257afabd01cc310ca0b4722bbd0d25bb1
Bill Bushey <wbushey@gmail.com>
2013-02-12 21:15:40 -0600

    Using a custom ViewletManager

---
+++ edem/group/messages/topicsdigest/interfaces.py
@@ -0,0 +1,5 @@
+# coding=utf-8
+from zope.viewlet.interfaces import IViewletManager
+
+class IDailyTopicsDigestHtmlVM(IViewletManager):
+    '''Viewlet Manager for E-Democracy's daily topics digest '''
