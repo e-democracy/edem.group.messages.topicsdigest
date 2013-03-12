f8a883820646e3a85175ff37bea036ee41e37128
William Bushey <wbushey@gmail.com>
2013-03-12 13:18:28 -0500

    Set a custom subject for the digests

--- edem/group/messages/topicsdigest/notifiers.py
+++ edem/group/messages/topicsdigest/notifiers.py
@@ -7,7 +7,22 @@ class DynamicTopicsDigestNotifier(Base):
 
     def __init__(self, group, request):
         super(DynamicTopicsDigestNotifier, self).__init__(group, request)
-        
+
+    @Lazy
+    def subject(self):
+        m = '[{groupShortName}] Digest: {new_posts} New, '\
+            '{new_topics} Topics - {a_subject}'
+        shortName = self.groupInfo.get_property('short_name',
+                                                self.groupInfo.name)
+        digestStats = self.topicsDigest.post_stats
+        a_subject = self.topicsDigest.topics[0]['topic_subject']
+        retval = m.format(groupShortName=shortName,
+                          new_posts=digestStats['new_posts'],
+                          new_topics=digestStats['new_topics'],
+                          a_subject=a_subject)
+        assert retval
+        return retval
+
     @Lazy
     def topicsDigest(self):
         retval = DailyTopicsDigest(self.context, self.siteInfo)
