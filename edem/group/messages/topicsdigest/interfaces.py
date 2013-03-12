316e21015f2e53633415637bf3d9ad698c66d276
William Bushey <wbushey@gmail.com>
2013-03-12 13:44:54 -0500

    Added reminder viewlet manager

--- edem/group/messages/topicsdigest/interfaces.py
+++ edem/group/messages/topicsdigest/interfaces.py
@@ -3,3 +3,6 @@ from zope.viewlet.interfaces import IViewletManager
 
 class IDailyTopicsDigestHtmlVM(IViewletManager):
     '''Viewlet Manager for E-Democracy's daily topics digest '''
+
+class IReminderTopicsDigestHtmlVM(IViewletManager):
+    '''Viewlet Manager for E-Democracy's reminder topics digest '''
