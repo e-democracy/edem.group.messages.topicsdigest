74d2bd3731d2df97d29537e73e5e2bb0182c1942
Bill Bushey <wbushey@gmail.com>
2013-03-15 08:18:21 -0500

    Created testing versions of the sites and groups_for_site methods in SendAllDigests Cleaned up setup.py, added requirements Added missing layer attributes in configure.zcml

--- edem/group/messages/topicsdigest/sendDigests.py
+++ edem/group/messages/topicsdigest/sendDigests.py
@@ -8,6 +8,23 @@ log = getLogger('edem.group.messages.topicsdigest.sendDigests')
 
 class SendAllDigests(Base):
 
+    @property
+    def sites(self):
+        '''A Testing version of the sites property'''
+        # For testing, we are only going to send digests to a small
+        # set of groups in a specific site.
+        site_root = self.context.site_root()
+        content = getattr(site_root, 'Content')
+        return [getattr(content, 'main')]
+
+    def groups_for_site(self, site):
+        '''A testing set of groups'''
+        # For testing, we are only going to send digests to a small
+        # set of groups. 
+        groups = getattr(site, 'groups')
+        return [getattr(groups, 'example'), getattr(groups, 'test')]
+
+
     @form.action(label=u'Send', failure='handle_send_all_digests_failure')
     def handle_send_all_digests(self, action, data):
         log.info('Processing the digests')
