975a3a8c3578ce89b1bddfde133153a29a74dce4
Bill Bushey <wbushey@gmail.com>
2013-06-05 11:19:46 -0500

    Commented out testing versions of sites and gorups_for_site

--- edem/group/messages/topicsdigest/sendDigests.py
+++ edem/group/messages/topicsdigest/sendDigests.py
@@ -8,30 +8,30 @@ log = getLogger('edem.group.messages.topicsdigest.sendDigests')
 
 class SendAllDigests(Base):
 
-    @property
-    def sites(self):
-        '''A Testing version of the sites property'''
-        # For testing, we are only going to send digests to a small
-        # set of groups in a specific site.
-        site_root = self.context.site_root()
-        content = getattr(site_root, 'Content')
-        retval = []
-        for site in ['main', 'initial_site']:
-            if hasattr(content, site):
-                retval.append(getattr(content, site))
-        return retval
 
-    def groups_for_site(self, site):
-        '''A testing set of groups'''
-        # For testing, we are only going to send digests to a small
-        # set of groups. 
-        groups = getattr(site, 'groups')
-        retval = []
-        for group in ['example', 'test']:
-            if hasattr(groups, group):
-                retval.append(getattr(groups, group))
-        return retval
-
+#    @property
+#    def sites(self):
+#        '''A Testing version of the sites property'''
+#        # For testing, we are only going to send digests to a small
+#        # set of groups in a specific site.
+#        site_root = self.context.site_root()
+#        content = getattr(site_root, 'Content')
+#        retval = []
+#        for site in ['main', 'initial_site']:
+#            if hasattr(content, site):
+#                retval.append(getattr(content, site))
+#        return retval
+#
+#    def groups_for_site(self, site):
+#        '''A testing set of groups'''
+#        # For testing, we are only going to send digests to a small
+#        # set of groups.
+#        groups = getattr(site, 'groups')
+#        retval = []
+#        for group in ['example', 'test']:
+#            if hasattr(groups, group):
+#                retval.append(getattr(groups, group))
+#        return retval
 
     @form.action(label=u'Send', failure='handle_send_all_digests_failure')
     def handle_send_all_digests(self, action, data):
@@ -47,4 +47,4 @@ class SendAllDigests(Base):
 
     def handle_send_all_digests_failure(self, action, data, errors):
         super(SendAllDigests, self).handle_send_all_digests_failure(action,
-                                    date, errors)
+                                    data, errors)
