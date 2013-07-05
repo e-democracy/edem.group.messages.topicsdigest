eee5f0e3cc62bb21b0efbceaa81b111a9012e21f
William Bushey <wbushey@gmail.com>
2013-07-05 17:02:24 -0500

    Rewrote query based on advice - hasn't been tested yet

--- edem/group/messages/topicsdigest/queries.py
+++ edem/group/messages/topicsdigest/queries.py
@@ -16,24 +16,31 @@ class DigestQuery(BaseDigestQuery):
     def topics_sinse_yesterday(self, siteId, groupId):
         yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
 
-        # SELECT topic.topic_id, topic.site_id, topic.group_id, 
+        # SELECT topic.topic_id, topic.site_id, topic.group_id,
         #       topic.original_subject, topic.num_posts, topic.first_post_id,
-        #       last_topic_post.post_id as last_post_id, 
-        #	    last_topic_post.date as last_post_date,  
-        #	    last_topic_post.user_id as last_author_id, 
-        #	    last_topic_post.body as last_post_body, 
-        #	    COUNT(all_new_topic_posts.*) as num_posts_day,
-        #	    oldest_new_topic_post.post_id as oldest_new_post_id,
-        #       topic_keywords_table.keywords
-        #   FROM topic, post as last_topic_post, 
-        #    	post as all_new_topic_posts, 
-        #   	post as oldest_new_topic_post
+        #       last_topic_post.post_id as last_post_id,
+        #       last_topic_post.date as last_post_date,
+        #       last_topic_post.user_id as last_author_id, 
+        #       last_topic_post.body as last_post_body,
+        #       (SELECT COUNT(1) 
+        #           FROM post as all_new_topic_posts 
+        #           WHERE all_new_topic_posts.topic_id = topic.topic_id 
+        #               AND all_new_topic_posts.date >= timestamp 'now' -
+        #                     interval '1 days') as num_posts_day,
+        #       oldest_new_topic_post.post_id as oldest_new_post_id,
+        #       topic_keywords.keywords
+        #  FROM topic
+        #       INNER JOIN topic_keywords 
+        #           ON topic_keywords.topic_id = topic.topic_id
+        #       INNER JOIN post as last_topic_post 
+        #           ON last_topic_post.topic_id = topic.topic_id
+        #       INNER JOIN post as oldest_new_topic_post 
+        #           ON oldest_new_topic_post.topic_id = topic.topic_id
 
         tt = self.topicTable            
         tkt = self.topicKeywordsTable         
         pt = self.postTable                   
         ltpt = pt.alias('last_topic_post')
-        antpt = pt.alias('all_new_topic_posts')
         ontpt = pt.alias('oldest_new_topic_post')
 
         cols = (tt.c.topic_id, tt.c.site_id, tt.c.group_id,
@@ -42,61 +49,41 @@ class DigestQuery(BaseDigestQuery):
                 ltpt.c.date.label('last_post_date'),
                 ltpt.c.user_id.label('last_author_id'),
                 ltpt.c.body.label('last_post_body'),
-                sa.func.count(antpt.c.post_id).label('num_posts_day'),
+                sa.func.count(pt.c.post_id)\
+                    .append_whereclause(pt.c.topic_id = tt.c.topic_id)\
+                    .append_whereclause(pt.c.date >= yesterday)\
+                    .label('num_posts_day'),
                 ontpt.c.post_id.label('oldest_new_post_id'),
                 tkt.c.keywords)
 
         s = sa.select(cols)
 
-        #   WHERE topic.site_id = 'initial_site'
-        #    	AND topic.group_id = 'example_group'
-        #    	AND topic.last_post_date >= timestamp 'yesterday'
+        s.join(tkt, tkt.c.topic_id == tt.c.topic_id)
+        s.join(ltpt, ltpt.c.topic_id == tt.c.topic_id)
+        s.join(ontpt, optpt.c.topic_id == tt.c.topic_id)
+
+        #  WHERE topic.site_id = 'main'
+        #       AND topic.group_id = 'mpls'
+        #       AND topic.last_post_date >= timestamp 'now' - interval '1 days'
+        #       AND last_topic_post.date = (
+        #           SELECT MAX(date)
+        #           FROM post
+        #           WHERE post.topic_id = topic.topic_id)
+        #       AND oldest_new_topic_post.date = (
+        #           SELECT MIN(date)
+        #           FROM post
+        #           WHERE post.topic_id = topic.topic_id
+        #               AND post.date >= timestamp 'now' - interval '1 days')
+
         s.append_whereclause(tt.c.site_id == siteId)
         s.append_whereclause(tt.c.group_id == groupId)
         s.append_whereclause(tt.c.last_post_date >= yesterday)
+        s.append_whereclause(ltpt.c.date == sa.func.max(pt.c.date)\
+            .append_whereclause(pt.c.topic_id == tt.c.topic_id))
+        s.append_whereclause(ontpt.c.date == sa.fun.min(pt.c.date)\
+            .append_whereclause(pt.c.topic_id == tt.c.topic_id)\
+            .append_whereclause(pt.c.date >= yesterday))
         
-        #	    AND last_topic_post.date in (SELECT MAX(date)
-        #					FROM post
-        #					WHERE post.topic_id = topic.topic_id
-        #					GROUP BY topic.topic_id)
-        s.append_whereclause(ltpt.c.date.in_(sa.select(
-                            [sa.func.max(pt.c.date)])\
-                        .where(pt.c.topic_id == tt.c.topic_id)\
-                        .group_by(tt.c.topic_id)
-                        ))
-
-        #     	AND all_new_topic_posts.topic_id = topic.topic_id
-        #   	AND all_new_topic_posts.date >= timestamp 'yesterday'
-        #   	AND oldest_new_topic_post.topic_id = topic.topic_id
-        s.append_whereclause(antpt.c.topic_id == tt.c.topic_id)
-        s.append_whereclause(antpt.c.date >= yesterday)
-        s.append_whereclause(ontpt.c.topic_id == tt.c.topic_id)
-
-        #   	AND oldest_new_topic_post.date in ( SELECT MIN(date)
-        #						FROM post
-        #						WHERE post.topic_id = topic.topic_id
-        #							AND post.date >= 'yesterday'
-        #						GROUP BY topic.topic_id)
-        s.append_whereclause(ontpt.c.date.in_(sa.select(
-                            [sa.func.min(pt.c.date)])\
-                        .where(sa.and_(pt.c.topic_id == tt.c.topic_id,
-                                pt.c.date >= yesterday))\
-                        .group_by(tt.c.topic_id)
-                        ))
-
-        #       AND topic.topic_id = topic_keyword_table.topic_id
-        s.append_whereclause(tt.c.topic_id == tkt.c.topic_id)
-
-        #    GROUP BY topic.topic_id, last_topic_post.user_id,
-        #       last_topic_post.body, last_topic_post.post_id, 
-        #       oldest_new_topic_post.post_id
-        s.append_group_by(tt.c.topic_id)
-        s.append_group_by(ltpt.c.user_id)
-        s.append_group_by(ltpt.c.body)
-        s.append_group_by(ltpt.c.post_id)
-        s.append_group_by(ontpt.c.post_id)
-        s.append_group_by(tkt.c.keywords)
-
         session = getSession()
         r = session.execute(s)
         retval = [{
