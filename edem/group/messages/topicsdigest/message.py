019dbc7998f0e521d8bc0d673b1277d0e4f127da
Bill Bushey <wbushey@gmail.com>
2013-07-09 13:02:45 -0500

    Pulled out setting the Reply-To header

--- edem/group/messages/topicsdigest/message.py
+++ edem/group/messages/topicsdigest/message.py
@@ -5,6 +5,9 @@ from gs.group.messages.topicsdigest.message import Message as Base
 class Message(Base):
 
     def add_headers(self, container, subject):
+        # A NOTE TO THE FUTURE:
+        # Do not set a Reply-To header. Bounces need to go back to the group,
+        # so that GroupServer can track bounces and adjust delivery as
+        # neccessary. 
         container = super(Message, self).add_headers(container, subject)
-        container['Reply-To'] = self.h('noreply@e-democracy.org')
         return container
