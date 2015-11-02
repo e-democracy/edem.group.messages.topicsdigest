d2e8c3c6ab93020f87e2934cefc900a88c18f669
Michael JasonSmith <mpj17@onlinegroups.net>
2015-11-02 09:24:14 -0800

    Switching the post-clip formatting to HTMLBody from markup_word.

--- edem/group/messages/topicsdigest/topicsDigest.py
+++ edem/group/messages/topicsdigest/topicsDigest.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 from __future__ import absolute_import, unicode_literals, print_function
 from zope.cachedescriptors.property import Lazy
-from gs.group.messages.post.text.postbody import escape_word, markup_uri, markup_www
+from gs.group.messages.text import HTMLBody
 from gs.group.messages.topic.digest.daily.topicsdigest import \
     DailyTopicsDigest as BaseDailyTopicsDigest
 from edem.group.messages.topic.digest.weekly.topicsdigest import \
@@ -34,14 +34,6 @@ class DailyTopicsDigest(BaseDailyTopicsDigest):
         retval = DigestQuery()
         return retval
 
-    @staticmethod
-    def markup_word(curr_word):
-        curr_word = escape_word(curr_word)
-        mu_word = markup_uri(None, curr_word, False, [])
-        if mu_word == curr_word:
-            mu_word = markup_www(None, curr_word, False, [])
-        return mu_word
-
     def format_topic(self, topic):
         topic = super(DailyTopicsDigest, self).format_topic(topic)
 
@@ -53,36 +45,17 @@ class DailyTopicsDigest(BaseDailyTopicsDigest):
             if not topic['last_post_clip'][-1].isspace():
                 topic['last_post_clip'] = \
                     topic['last_post_clip'].rsplit(None, 1)[0]
-
-        else:
+        else:  # len(topic['last_post_body']) <= CLIP_LENGTH:
             topic['last_post_more_available'] = False
             topic['last_post_clip'] = topic['last_post_body']
 
-        # Markup Links with a loop inspired by
-        # gs.group.messages.post.postbody.markup_email.
-        # HACK: The markup functions in gs.group.messages.post take a
-        # ContentProvider as an argument, but most do not actually reference
-        # it. Since we don't have a ContentProvider here, we'll just exploit
-        # the non-use of the argument.
-        marked_up_clip = ''
-        curr_word = ''
-
-        for char in topic['last_post_clip']:
-            if char.isspace():
-                if curr_word:
-                    mu_word = self.markup_word(curr_word)
-                    curr_word = ''
-                    marked_up_clip += mu_word
-                if char == '\n':
-                    char = '<br/>'
-                marked_up_clip += char
-            else:
-                curr_word += char
-        if curr_word:
-            mu_word = self.markup_word(curr_word)
-            marked_up_clip += mu_word
-
-        topic['last_post_clip'] = marked_up_clip
+        # --=mpj17=-- Use the standard *email* formatting for the
+        # post-clip
+        assert len(topic['last_post_clip']) <= CLIP_LENGTH, 'Clip too long'
+        htmlBody = HTMLBody(topic['last_post_clip'])
+        markedUpClip = unicode(htmlBody)
+        assert len(markedUpClip) >= len(topic['last_post_clip']), 'Markup destroyed info'
+        topic['last_post_clip'] = markedUpClip
         return topic
 
 
